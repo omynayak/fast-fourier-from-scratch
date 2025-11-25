363c593f517ed4352417d1858814a7875a3e8a16
omynayak <manelomnayak099@gmail.com>
2025-11-26 03:34:09 +0530

    Used dyn_arr in fft.cpp

--- fft.cpp
+++ fft.cpp
@@ -1,40 +1,35 @@
 #include "complex_header.h"
 
-std::vector<comp::Complex<double>> fft(const std::vector<double>& values)
+comp::dyn_arr<comp::Complex<double>> fft(const comp::dyn_arr<double>& values)
 {
     int N = values.size();
 
 
     if (N == 1) {
-        return { comp::Complex<double>(values[0], 0.0) };
+        comp::dyn_arr<comp::Complex<double>> result(1);
+        result[0] = comp::Complex<double>(values[0], 0.0);
+        return result;
     }
 
-    std::vector<double> even_vals(N/2);
-    std::vector<double> odd_vals(N/2);
+    comp::dyn_arr<double> even_vals(N / 2);
+    comp::dyn_arr<double> odd_vals(N / 2);
 
-    for (int i = 0; i < N/2; i++) {
-        even_vals[i] = values[2*i];
-        odd_vals[i]  = values[2*i + 1];
+    for (int i = 0; i < N / 2; i++) {
+        even_vals[i] = values[2 * i];
+        odd_vals[i]  = values[2 * i + 1];
     }
 
+    auto E = fft(even_vals);
+    auto O = fft(odd_vals);
 
-    auto E = fft(even_vals);  
-    auto O = fft(odd_vals);   
+    comp::dyn_arr<comp::Complex<double>> X(N);
 
-
-    std::vector<comp::Complex<double>> X(N);
-
-
-    for (int k = 0; k < N/2; k++) {
-
-
+    for (int k = 0; k < N / 2; k++) {
         double angle = -2.0 * M_PI * k / N;
-
-
         comp::Complex<double> W = twiddle(angle);
 
         X[k]      = E[k] + W * O[k];
-        X[k+N/2]  = E[k] - W * O[k];
+        X[k + N / 2] = E[k] - W * O[k];
     }
 
     return X;
